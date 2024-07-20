4071a77a92e0550e277c755286e436119fc48034
Kraven Thommel <yourgentarevalo44@gmail.com>
2024-07-20 11:01:21 -0600

    h

--- src/main/java/umg/programacionII/PKG_Grupo1/ClsGrupo1.java
+++ src/main/java/umg/programacionII/PKG_Grupo1/ClsGrupo1.java
@@ -1,10 +1,19 @@
 package umg.programacionII.PKG_Grupo1;
 
 public class ClsGrupo1 {
+    public float pi=3.1416f;
+
 
     public float radio(float radio){
         float area,pi=3.1416f;
         return pi*(radio*radio);
     }
+    public float circunferencia(float radio){
+        return 2*pi*radio;
+    }
+    public float volumencubo(float lados){
+        return lados*lados*lados;
+    }
+
 
 }
--- src/main/java/umg/programacionII/PKG_Grupo2/ClsGrupo2.java
+++ src/main/java/umg/programacionII/PKG_Grupo2/ClsGrupo2.java
@@ -4,6 +4,10 @@ public class ClsGrupo2 {
     public float triarea(float base,float altura){
         return (base*altura)/2;
     }
-
-
+    public float piravol(float base,float altura){
+        return (base*altura)/3;
+    }
+    public float repiaria(float base,float altura){
+        return (base*altura)/2;
+    }
 }
